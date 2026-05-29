69b2d33b23c4dfe332d55a81a3c69093440369cc
Emmanuel Niyogakiza <mac@macs-MacBook-Pro.local>
2026-05-29 10:30:07 +0200

    creating another pages

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-let elementItems = document.querySelectorAll(".big");
-for(let item of elementItems){
+let elementItems = document.querySelectorAll(".big a");
+
+for (let item of elementItems) {
+
     item.addEventListener('mouseenter', () => {
         item.style.backgroundColor = 'black';
         item.style.color = 'blue';
         item.style.cursor = 'pointer';
-    })
+    });
+
     item.addEventListener('mouseleave', () => {
         item.style.backgroundColor = '';
         item.style.color = '';
+    });
+    item.addEventListener('click', ()=>{
+        alert("You have clicked")
     })
+
 }
 
